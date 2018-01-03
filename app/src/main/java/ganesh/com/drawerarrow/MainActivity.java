2db5a8f0e4b47454bf423d86372ab5f4be5a5093
Your Name <you@example.com>
2018-01-03 11:51:32 +0530

    first commit

---
+++ app/src/main/java/ganesh/com/drawerarrow/MainActivity.java
@@ -0,0 +1,80 @@
+package ganesh.com.drawerarrow;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.view.Gravity.START;
+public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerArrowDrawable drawerArrowDrawable;
+    private float offset;
+    private boolean flipped;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ImageView imageView = (ImageView) findViewById(R.id.drawer_indicator);
+        final Resources resources = getResources();
+
+        drawerArrowDrawable = new DrawerArrowDrawable(resources);
+        drawerArrowDrawable.setStrokeColor(resources.getColor(R.color.light_gray));
+        imageView.setImageDrawable(drawerArrowDrawable);
+
+        drawer.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override public void onDrawerSlide(View drawerView, float slideOffset) {
+                offset = slideOffset;
+
+                // Sometimes slideOffset ends up so close to but not quite 1 or 0.
+                if (slideOffset >= .995) {
+                    flipped = true;
+                    drawerArrowDrawable.setFlip(flipped);
+                } else if (slideOffset <= .005) {
+                    flipped = false;
+                    drawerArrowDrawable.setFlip(flipped);
+                }
+
+                drawerArrowDrawable.setParameter(offset);
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if (drawer.isDrawerVisible(START)) {
+                    drawer.closeDrawer(START);
+                } else {
+                    drawer.openDrawer(START);
+                }
+            }
+        });
+
+        final TextView styleButton = (TextView) findViewById(R.id.indicator_style);
+        styleButton.setOnClickListener(new View.OnClickListener() {
+            boolean rounded = false;
+
+            @Override public void onClick(View v) {
+                styleButton.setText(rounded //
+                        ? resources.getString(R.string.rounded) //
+                        : resources.getString(R.string.squared));
+
+                rounded = !rounded;
+
+                drawerArrowDrawable = new DrawerArrowDrawable(resources, rounded);
+                drawerArrowDrawable.setParameter(offset);
+                drawerArrowDrawable.setFlip(flipped);
+                drawerArrowDrawable.setStrokeColor(resources.getColor(R.color.light_gray));
+
+                imageView.setImageDrawable(drawerArrowDrawable);
+            }
+        });
+    }
+}
